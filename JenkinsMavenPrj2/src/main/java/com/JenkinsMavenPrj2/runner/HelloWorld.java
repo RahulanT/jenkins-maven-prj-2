46fa78d980772d71dca353bb94f88895a423d092
Rahulan Thevkanthan <rahulan.thevakanthan@fdmgroup.com>
2024-04-09 10:44:57 -0400

    first commit

---
+++ JenkinsMavenPrj2/src/main/java/com/JenkinsMavenPrj2/runner/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.JenkinsMavenPrj2.runner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ! This is from Jenkins Maven Project 2 !");
+	}
+
+}
